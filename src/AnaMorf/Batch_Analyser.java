aee9dcbf3bb1eb78f1ad18e5ec2498e9a235a87a
davejpbarry <davejpbarry@gmail.com>
2017-12-06 15:51:19 +0000

    Slight correction in Batch_Analyser.analyse

--- src/AnaMorf/Batch_Analyser.java
+++ src/AnaMorf/Batch_Analyser.java
@@ -246,7 +246,7 @@ public class Batch_Analyser implements PlugIn {
                     if (gui.isCreateMasks()) {
                         IJ.saveAs(skelOutput, "png", resultsDirectory + "//" + skelOutput.getTitle());
                         double[][] skelVals = SkeletonProcessor.mapSkeleton(currImage.getProcessor(), bwSkelImage, FOREGROUND);
-                        DataWriter.saveValues(skelVals, new File(String.format("%s%s%s - SkeletonMap.csv", resultsDirectory,File.separator,imageName)), new String[]{"X", "Y", "Z"});
+                        DataWriter.saveValues(skelVals, new File(String.format("%s%s%s - SkeletonMap.csv", resultsDirectory,File.separator,imageName)), new String[]{"X", "Y", "Z"}, null);
                     }
                 }
 //            }
